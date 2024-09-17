78daea875e4e19eb3fff602a3c14d7b865a732be
Skelonv3 <patryk.chudzia@gmail.com>
2024-09-17 13:07:07 +0200

    Points feature

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 "use strict";
 
 const squares = document.querySelectorAll(".square");
+let labelPoints = document.querySelector('.label-points');
 const checkClasses = function () {
   squares.forEach((square) => {
     if (
       square.classList.contains("point") &&
       square.classList.contains("player")
     ) {
-      console.log(square.querySelector("item"));
+    player.points++;
+    labelPoints.textContent = `Points: ${player.points}`;
+      placePoints();
       square.querySelector("item").style.backgroundColor = "rgb(242, 162, 33)";
     } else if (square.classList.contains("player")) {
-      console.log(square.querySelector("item"));
       square.querySelector("item").style.backgroundColor = "rgb(242, 216, 18)";
     } else if (square.classList.contains("point")) {
-      console.log(square.querySelector("item"));
       square.querySelector("item").style.backgroundColor = "rgb(41, 190, 46)";
     } else if (square.classList.contains("enemy")) {
-      console.log(square.querySelector("item"));
       square.querySelector("item").style.backgroundColor = "rgb(244, 40, 40)";
     } else {
       square.querySelector("item").style.backgroundColor = "rgb(255, 255, 255)";
     }
   });
 };
+function placePoints() {
+  squares.forEach((square) => {
+    square.classList.contains("point") ? square.classList.remove("point") : "";
+  });
+  squares[
+    Math.trunc(Math.random() * 10) * 10 + Math.trunc(Math.random() * 10)
+  ].classList.add("point");
+}
+placePoints();
 const player = {
   x: Math.trunc(Math.random() * 10),
   y: Math.trunc(Math.random() * 10),
-  point: 0,
+  points: 0,
   alive: true,
   placePlayer: function () {
     squares.forEach((square) => {
