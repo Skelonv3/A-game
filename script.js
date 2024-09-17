c27a32cc14c24d658e9e453b415a9ee9ad0c5e72
Skelonv3 <patryk.chudzia@gmail.com>
2024-09-17 10:36:13 +0200

    Added board + squares changing color based on class name

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+
+const checkClasses = function () {
+  [...document.querySelectorAll(".square")].forEach((square) => {
+    if (
+      square.classList.contains("point") &&
+      square.classList.contains("player")
+    ) {
+      console.log(square.querySelector("item"));
+      square.querySelector("item").style.backgroundColor = 'rgb(242, 162, 33)';
+    } else if (square.classList.contains("player")) {
+      console.log(square.querySelector("item"));
+      square.querySelector("item").style.backgroundColor = 'rgb(242, 216, 18)';
+    } else if (square.classList.contains("point")) {
+      console.log(square.querySelector("item"));
+      square.querySelector("item").style.backgroundColor = 'rgb(41, 190, 46)';
+    } else if (square.classList.contains("enemy")) {
+      console.log(square.querySelector("item"));
+      square.querySelector("item").style.backgroundColor = 'rgb(244, 40, 40)';
+    }
+  });
+};
+checkClasses();
