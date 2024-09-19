f93b6c993f2a216ae1547c8dfda6de5a725cffd8
Skelonv3 <patryk.chudzia@gmail.com>
2024-09-19 18:57:25 +0200

    increasing snake's length + You can die now!

--- script.js
+++ script.js
@@ -1,73 +1,115 @@
 "use strict";
 
 const squares = document.querySelectorAll(".square");
-let labelPoints = document.querySelector('.label-points');
-const checkClasses = function () {
-  squares.forEach((square) => {
-    if (
-      square.classList.contains("point") &&
-      square.classList.contains("player")
-    ) {
-    player.points++;
-    labelPoints.textContent = `Points: ${player.points}`;
-      placePoints();
-      square.querySelector("item").style.backgroundColor = "rgb(242, 162, 33)";
-    } else if (square.classList.contains("player")) {
-      square.querySelector("item").style.backgroundColor = "rgb(242, 216, 18)";
-    } else if (square.classList.contains("point")) {
-      square.querySelector("item").style.backgroundColor = "rgb(41, 190, 46)";
-    } else if (square.classList.contains("enemy")) {
-      square.querySelector("item").style.backgroundColor = "rgb(244, 40, 40)";
-    } else {
-      square.querySelector("item").style.backgroundColor = "rgb(255, 255, 255)";
-    }
-  });
+const labelPoints = document.querySelector(".label-points");
+const labelGameOver = document.querySelector(".label-game_over");
+
+let length = 1;
+let turnsAfterScoringPoint = -1;
+const pointCoordinates = [];
+
+const pointScored = function () {
+  player.points++;
+  length++;
+  labelPoints.textContent = `Points: ${player.points}`;
 };
-function placePoints() {
-  squares.forEach((square) => {
-    square.classList.contains("point") ? square.classList.remove("point") : "";
+const placePoint = function (squaresArray) {
+    pointCoordinates[0] = Math.trunc(Math.random() * 10);
+    pointCoordinates[1] = Math.trunc(Math.random() * 10);
+    console.log(pointCoordinates[0], pointCoordinates[1]);
+  squaresArray.forEach((square) => {
+    square.querySelector("item").classList.contains("point")
+      ? square.querySelector("item").classList.remove("point")
+      : "";
   });
-  squares[
-    Math.trunc(Math.random() * 10) * 10 + Math.trunc(Math.random() * 10)
-  ].classList.add("point");
-}
-placePoints();
+  squares[pointCoordinates[1] * 10 + pointCoordinates[0]]
+    .querySelector("item")
+    .classList.add("point");
+};
+const move = function (e) {
+  for (let i = length - 1; i >= 0; i--) {
+    if (i === 0) {
+      if (e.key === "w") {
+        if (player.y[0] === 0) {
+          player.alive = false;
+        } else {
+          player.y[0]--;
+        }
+      } else if (e.key === "s") {
+        if (player.y[0] === 9) {
+          player.alive = false;
+        } else {
+          player.y[0]++;
+        }
+      } else if (e.key === "a") {
+        if (player.x[0] === 0) {
+          player.alive = false;
+        } else {
+          player.x[0]--;
+        }
+      } else if (e.key === "d") {
+        if (player.x[0] === 9) {
+          player.alive = false;
+        } else {
+          player.x[0]++;
+        }
+      }
+    } else if (i > 0) {
+      if (
+        (e.key === "w" && player.y[0] === 0) ||
+        (e.key === "s" && player.y[0] === 9) ||
+        (e.key === "a" && player.x[0] === 0) ||
+        (e.key === "d" && player.x[0] === 9)
+      ) {
+      } else {
+        player.x[i] = player.x[i - 1];
+        player.y[i] = player.y[i - 1];
+      }
+    }
+  }
+  for (let i = 0; i < length; i++) {
+    if (player.x[0] === player.x[i] && player.y[0] === player.y[i] && i > 2) {
+      player.alive = false;
+    }
+    if(pointCoordinates[0] === player.x[i] && pointCoordinates[1] === player.y[i] && i > 0) {
+      placePoint(squares);
+    }
+  }
+  console.log(player.x, player.y);
+
+  if (!player.alive) {
+    document.removeEventListener("keydown", move);
+    labelGameOver.style.display = "flex";
+  }
+  console.log(player.alive);
+  player.placePlayer();
+  if (
+    player.x[0] === pointCoordinates[0] &&
+    player.y[0] === pointCoordinates[1]
+  ) {
+    pointScored();
+    placePoint(squares);
+  }
+};
+
 const player = {
-  x: Math.trunc(Math.random() * 10),
-  y: Math.trunc(Math.random() * 10),
+  x: [Math.trunc(Math.random() * 10)],
+  y: [Math.trunc(Math.random() * 10)],
   points: 0,
   alive: true,
   placePlayer: function () {
     squares.forEach((square) => {
-      square.classList.contains("player")
-        ? square.classList.remove("player")
+      square.querySelector("item").classList.contains("player")
+        ? square.querySelector("item").classList.remove("player")
         : "";
     });
-    squares[this.y * 10 + this.x].classList.add("player");
+    for (let i = 0; i < length; i++) {
+      squares[this.y[i] * 10 + this.x[i]]
+        .querySelector("item")
+        .classList.add("player");
+    }
   },
 };
+placePoint(squares);
 player.placePlayer();
-
-document.addEventListener("keydown", function (e) {
-  console.log(player.x, player.y);
-  if (e.key === "w" && player.y > 0) {
-    player.y--;
-    player.placePlayer();
-    checkClasses();
-  } else if (e.key === "s" && player.y < 9) {
-    player.y++;
-    player.placePlayer();
-    checkClasses();
-  } else if (e.key === "a" && player.x > 0) {
-    player.x--;
-    player.placePlayer();
-    checkClasses();
-  } else if (e.key === "d" && player.x < 9) {
-    player.x++;
-    player.placePlayer();
-    checkClasses();
-  }
-});
-
-console.log(player);
-checkClasses();
+document.addEventListener("keydown", move);
